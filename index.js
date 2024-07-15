b949f3a15574dd310124e34e32b7080b7b1cd7ed
Umair Bin Nasir <aumair1259@gmail.com>
2024-07-15 02:11:56 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+let generateNumber = Math.floor(Math.random() * 10);
+let answer = await inquirer.prompt({
+    type: "number",
+    message: "Guess a number between 0 and 10",
+    name: "userGuess"
+});
+if (answer.userGuess === generateNumber) {
+    console.log("Conguratulations you guessed right");
+}
+else {
+    console.log(`Try Again! Correct number was ${generateNumber}`);
+}
